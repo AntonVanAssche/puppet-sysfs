526a1f0aaca2ed80dbd213c73a4d06365e9eb3e9
Anton Van Assche <vanasscheanton@gmail.com>
2024-12-12 14:11:32 +0100

    Use is_expected.to instead of should
    Fixes the following pdk error:
      pdk (CONVENTION): rubocop: Prefer `is_expected.to` over `should`. (spec/classes/init_spec.rb:5:10)

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 describe 'sysfs' do
 
   context 'with defaults for all parameters' do
-    it { should contain_class('sysfs') }
+    it { is_expected.to contain_class('sysfs') }
   end
 end
