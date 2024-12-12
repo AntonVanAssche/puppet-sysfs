a020710b7a70bed37fbc0fc94d492c6fb742da56
Anton Van Assche <vanasscheanton@gmail.com>
2024-12-12 12:58:52 +0100

    [setting] Add structured documentation

--- manifests/setting.pp
+++ manifests/setting.pp
@@ -1,3 +1,13 @@
+# @summary Set sysfs settings
+#
+# @example A basic example:
+#   sysfs::setting { 'class/block/sdb/queue/read_ahead_kb':
+#     value => 8
+#   }
+#
+# @param value
+#   The value to set the sysfs setting to.
+#
 define sysfs::setting($value) {
   concat::fragment { "${name}":
     target => "/etc/sysfs.conf",
