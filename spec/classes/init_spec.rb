6f6687019a6f5ef40c45854202004485772564da
Anton Van Assche <vanasscheanton@gmail.com>
2024-12-12 18:57:31 +0100

    Set os facts

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
+
 describe 'sysfs' do
-  context 'with defaults for all parameters' do
-    it { is_expected.to contain_class('sysfs') }
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile.with_all_deps }
+      it { is_expected.to contain_class('sysfs') }
+    end
   end
 end
